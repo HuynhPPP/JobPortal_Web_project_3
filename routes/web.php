c56af561fe715d25086b6a31011dc1f61adcde87
HuynhPPP <tankhuong02@gmail.com>
2024-09-15 07:20:54 +0700

    layout

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AccountController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/account/register', [AccountController::class, 'registration'])->name('account.registration');
+Route::post('/account/process-register', [AccountController::class, 'processRegistration'])->name('account.processRegistration');
+Route::get('/account/login', [AccountController::class, 'login'])->name('account.login');
+
