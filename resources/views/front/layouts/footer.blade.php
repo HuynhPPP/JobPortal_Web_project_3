c540083f31dfa2d5a2dc05e5f7fd212df2d989f6
HuynhPPP <tankhuong02@gmail.com>
2024-10-13 23:21:42 +0700

    update-footer

--- resources/views/front/layouts/footer.blade.php
+++ resources/views/front/layouts/footer.blade.php
@@ -1,5 +1,76 @@
-<footer class="bg-dark py-3 bg-2">
-    <div class="container">
-      <p class="text-center text-white pt-3 fw-bold fs-6">© 2023 xyz company, all right reserved</p>
+<head>
+  <link rel="stylesheet" type="text/css" href="{{ asset("assets/user/css/footer.css") }}" />
+</head>
+
+
+<footer class="bg-dark py-3 bg-2" id="tempaltemo_footer">
+  <div class="container">
+    <div class="row">
+
+        <div class="col-md-6 pt-5">
+            <h2 class="h2 text-success border-bottom pb-3 border-light logo">TopWork</h2>
+            <ul class="list-unstyled text-light footer-link-list">
+                <li>
+                    <i class="fas fa-map-marker-alt fa-fw"></i>
+                    123 Consectetur at ligula 10660
+                </li>
+                <li>
+                    <i class="fa fa-phone fa-fw"></i>
+                    <a class="text-decoration-none" href="tel:010-020-0340">010-020-0340</a>
+                </li>
+                <li>
+                    <i class="fa fa-envelope fa-fw"></i>
+                    <a class="text-decoration-none" href="mailto:info@company.com">info@company.com</a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="col-md-6 pt-5">
+            <h2 class="h2 text-light border-bottom pb-3 border-light">Further Info</h2>
+            <ul class="list-unstyled text-light footer-link-list">
+                <li><a class="text-decoration-none" href="#">Home</a></li>
+                <li><a class="text-decoration-none" href="#">About Us</a></li>
+                <li><a class="text-decoration-none" href="#">Shop Locations</a></li>
+                <li><a class="text-decoration-none" href="#">FAQs</a></li>
+                <li><a class="text-decoration-none" href="#">Contact</a></li>
+            </ul>
+        </div>
+
     </div>
+
+    <div class="row text-light mb-4">
+        <div class="col-12 mb-3">
+            <div class="w-100 my-3 border-top border-light"></div>
+        </div>
+        <div class="col-auto me-auto">
+            <ul class="list-inline text-left footer-icons">
+                <li class="list-inline-item border border-light rounded-circle text-center">
+                    <a class="text-light text-decoration-none" target="_blank" href="http://facebook.com/"><i class="fa fa-facebook-f fa-lg fa-fw"></i></a>
+                </li>
+                <li class="list-inline-item border border-light rounded-circle text-center">
+                    <a class="text-light text-decoration-none" target="_blank" href="https://www.instagram.com/"><i class="fa fa-instagram fa-lg fa-fw"></i></a>
+                </li>
+                <li class="list-inline-item border border-light rounded-circle text-center">
+                    <a class="text-light text-decoration-none" target="_blank" href="https://twitter.com/"><i class="fa fa-twitter fa-lg fa-fw"></i></a>
+                </li>
+                <li class="list-inline-item border border-light rounded-circle text-center">
+                    <a class="text-light text-decoration-none" target="_blank" href="https://www.linkedin.com/"><i class="fa fa-linkedin fa-lg fa-fw"></i></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+<div class="w-100 py-3" style="background-color: #1d242d !important">
+    <div class="container">
+        <div class="row pt-2">
+            <div class="col-12">
+                <p class="text-left text-light">
+                    Copyright &copy; 2024 Company Name 
+                    | Designed by <a rel="sponsored" href="https://templatemo.com" target="_blank">TemplateMo</a>
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
 </footer>
